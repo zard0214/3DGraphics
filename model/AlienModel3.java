5371ea5690ce6716f9221c18f829db1e6c3dbd02
Zhecheng Zhao <zzhao84@sheffield.ac.uk>
2023-10-28 07:56:59 +0100

    animation issues

--- model/AlienModel3.java
+++ model/AlienModel3.java
@@ -30,11 +30,16 @@ public class AlienModel3 {
     private boolean roll = false;
     private Model floor, sphere, cube, cube2;
     private SGNode alienRoot;
-    private TransformNode translateX, bodyMoveTranslate, bodyRotateTranslate, bodyTransform, bodyTranslate, bodyRotate, headMoveTranslate, headTransform, headTranslate, headRotate;
+    private TransformNode translateX;
+    private TransformNode bodyMoveTranslate, bodyRotateTranslate, bodyTransform, bodyTranslate, bodyRotate;
+    private TransformNode headMoveTranslate, headTransform, headTranslate, headRotate;
+    private TransformNode antennaBottomMoveTranslate, antennaBottomTransform, antennaBottomTranslate, antennaBottomRotate;
+    private TransformNode antennaTopMoveTranslate, antennaTopTransform, antennaTopTranslate, antennaTopRotate;
+    private TransformNode bodyRoll, headRoll, antennaBottomRoll, antennaTopRoll;
     private float xPosition = 0;
 
-    private NameNode body, head;
-    private ModelNode bodyShape, headShape;
+    private NameNode body, head, antennaBottom, antennaTop;
+    private ModelNode bodyShape, headShape, antennaBottomShape, antennaTopShape;
     public AlienModel3(GL3 gl, Camera camera, Light light_1, Light light_2, SpotLight spotLight, AlienShader alienShader, Material alienMaterial, Mat4 mat4, Mesh m, int[] alienTexture, Mat4 transition) {
 
         int[] alientextureId0 = TextureLibrary.loadTexture(gl, Constant.ALIEN_TEXTURE_GRAY);
@@ -55,28 +60,32 @@ public class AlienModel3 {
         float bodyWidth = 1.7f;
         float bodyDepth = 1.7f;
         float headScale = 1.3f;
-        float armLength = 3.5f;
-        float armScale = 0.5f;
+        float antennaBottomScale1 = 0.125f;
+        float antennaBottomScale2 = 0.5f;
+        float antennaTopScale2 = 0.18f;
         float legLength = 3.4f;
-        float legScale = 0.0f;
 
+        /***********  body  ***************/
         alienRoot = new NameNode("root");
         bodyRotateTranslate = new TransformNode("alien transform", Mat4Transform.rotateAroundY(0));
 
         bodyTranslate = new TransformNode("alien body transform2",Mat4Transform.translate(0, legLength,0));
         bodyRotate = new TransformNode("alien body bodyRotate", Mat4Transform.rotateAroundY(0));
 
-        body = new NameNode("body");
+
         body = new NameNode("body");
         bodyMoveTranslate = new TransformNode("alien body transform", Mat4Transform.translate(xPosition,0,0));
+        bodyRoll = new TransformNode("alien roll",  Mat4Transform.rotateAroundY(0));
 
         Mat4 m2 = Mat4Transform.scale(bodyWidth, bodyHeight, bodyDepth);
         m2 = Mat4.multiply(m2, Mat4Transform.translate(0,-1.5f,0));
         bodyTransform = new TransformNode("body transform", m2);
         bodyShape = new ModelNode("Alien(body)", cube);
 
+        /***********  head  ***************/
         head = new NameNode("head");
         headMoveTranslate = new TransformNode("alien head transform",Mat4Transform.translate(xPosition,0,0));
+        headRoll = new TransformNode("head roll",  Mat4Transform.rotateAroundY(0));
 
         headTranslate = new TransformNode("alien head transform2",Mat4Transform.translate(0, 0,0));
         headRotate = new TransformNode("alien head rotate",  Mat4Transform.rotateAroundY(0));
@@ -88,20 +97,48 @@ public class AlienModel3 {
         headTransform = new TransformNode("head transform2", m2);
         headShape = new ModelNode("Alien(head)", sphere);
 
-//        alienRoot.addChild(bodyMoveTranslate);
-//        bodyMoveTranslate.addChild(bodyTranslate);
-//        bodyTranslate.addChild(body);
-//        body.addChild(bodyTransform);
-//        bodyTransform.addChild(bodyShape);
-//        body.addChild(head);
-//        head.addChild(headTransform);
-//        headTransform.addChild(headShape);
+        /***********  antenna_bottom  ***************/
+        antennaBottom = new NameNode("antennaBottom");
+        antennaBottomMoveTranslate = new TransformNode("alien antenna_bottom transform",Mat4Transform.translate(xPosition,0,0));
+        antennaBottomRoll = new TransformNode("antennaBottom roll",  Mat4Transform.rotateAroundY(0));
 
+        antennaBottomTranslate = new TransformNode("alien antennaBottom transform2",Mat4Transform.translate(0, 0,0));
+        antennaBottomRotate = new TransformNode("alien antennaBottom rotate",  Mat4Transform.rotateAroundY(0));
+
+        m2 = new Mat4(1);
+        m2 = Mat4.multiply(m2, Mat4Transform.translate(0, bodyHeight,0));
+        m2 = Mat4.multiply(m2, Mat4Transform.scale(antennaBottomScale1, antennaBottomScale2, antennaBottomScale1));
+        m2 = Mat4.multiply(m2, Mat4Transform.translate(0, -3.80f,0));
+        antennaBottomTransform = new TransformNode("antennaBottom transform2", m2);
+        antennaBottomShape = new ModelNode("Alien(antennaBottom)", sphere);
+
+        /***********  antenna_top  ***************/
+        antennaTop = new NameNode("antennaTop");
+        antennaTopMoveTranslate = new TransformNode("alien antenna_top transform",Mat4Transform.translate(xPosition,0,0));
+        antennaTopRoll = new TransformNode("antennaTop roll",  Mat4Transform.rotateAroundY(0));
+
+        antennaTopTranslate = new TransformNode("alien antennaTop transform2",Mat4Transform.translate(0, 0,0));
+        antennaTopRotate = new TransformNode("alien antennaTop rotate",  Mat4Transform.rotateAroundY(0));
+
+        m2 = new Mat4(1);
+        m2 = Mat4.multiply(m2, Mat4Transform.translate(0, bodyHeight,0));
+        m2 = Mat4.multiply(m2, Mat4Transform.scale(antennaTopScale2, antennaTopScale2, antennaTopScale2));
+        m2 = Mat4.multiply(m2, Mat4Transform.translate(0, -8.8f,0));
+        antennaTopTransform = new TransformNode("antennaTop transform2", m2);
+        antennaTopShape = new ModelNode("Alien(antennaTop)", sphere);
+
+        /***********  lefteye  ***************/
+        /***********  righteye  ***************/
+        /***********  leftarm  ***************/
+        /***********  rightarm  ***************/
+        /***********  leftear  ***************/
+        /***********  rightear  ***************/
         createNode();
         alienRoot.update();
     }
 
     private void createNode() {
+        /*****  body  ******/
         alienRoot.addChild(bodyRotateTranslate);
         bodyRotateTranslate.addChild(bodyMoveTranslate);
         bodyMoveTranslate.addChild(body);
@@ -109,25 +146,35 @@ public class AlienModel3 {
         body.addChild(bodyTranslate);
         bodyTranslate.addChild(bodyTransform);
         bodyTransform.addChild(bodyShape);
-    }
 
-//    private void createNode() {
-//        alienRoot.addChild(bodyMoveTranslate);
-//            bodyMoveTranslate.addChild(bodyTranslate);
-//                bodyTranslate.addChild(body);
-//                    body.addChild(bodyTransform);
-//                        bodyTransform.addChild(bodyShape);
-//
-//        headShape.addChild(head);
-//
-//        head.addChild(headMoveTranslate);
-//            headMoveTranslate.addChild(headTranslate);
-//                headTranslate.addChild(head);
-//                    head.addChild(headTransform);
-//                        headTransform.addChild(headShape);
-//
-//
-//    }
+        /*****  head  ******/
+        bodyTranslate.addChild(bodyRoll);
+        bodyRoll.addChild(head);
+        head.addChild(headRotate);
+        headRotate.addChild(headMoveTranslate);
+
+        head.addChild(headTranslate);
+        headTranslate.addChild(headTransform);
+        headTransform.addChild(headShape);
+
+        /*****  antennaBottom  ******/
+        headTranslate.addChild(headRoll);
+        headRoll.addChild(antennaBottom);
+        antennaBottom.addChild(antennaBottomRotate);
+
+        antennaBottomRotate.addChild(antennaBottomTranslate);
+        antennaBottomTranslate.addChild(antennaBottomTransform);
+        antennaBottomTransform.addChild(antennaBottomShape);
+
+        /*****  antennaTop  ******/
+        antennaBottomTranslate.addChild(antennaBottomRoll);
+        antennaBottomRoll.addChild(antennaTop);
+        antennaTop.addChild(antennaTopRotate);
+
+        antennaTopRotate.addChild(antennaTopTranslate);
+        antennaTopTranslate.addChild(antennaTopTransform);
+        antennaTopTransform.addChild(antennaTopShape);
+    }
 
     public void render(GL3 gl) {
         if (rock) rockAroundZ();
