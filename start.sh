621ef7d1a30a3d99e11e8c1ccac5ca75b3d9291b
Zhecheng Zhao <zzhao84@sheffield.ac.uk>
2023-11-09 17:06:32 +0000

    start shell file

---
+++ start.sh
@@ -0,0 +1,13 @@
+javac core/camera/*.java
+javac core/light/*.java
+javac core/shaders/*.java
+javac core/*.java
+javac gmaths/*.java
+javac model/basic/*.java
+javac model/node/*.java
+javac model/*.java
+javac util/*.java
+
+javac *.java
+
+java Aliens
